d5d307a4abfd57662c2e8fd900afb35819cde698
tim <timohide13@gmail.com>
2022-10-01 15:43:27 +0300

    'lol'

--- mypics/webscraping.py
+++ mypics/webscraping.py
@@ -8,11 +8,15 @@ from selenium.webdriver.chrome.options import Options
 
 def russian_youtube():
 
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
 
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 
     chrome_options.add_argument("--headless")
+
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    chrome_options.add_argument("--no-sandbox")
 
     driver = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=chrome_options)
 
